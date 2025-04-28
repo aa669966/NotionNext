110dbe222e41a5a08e08c6ccd6cac54b6cd33c08
aa669966 <wwd5203344@alu.msu.edu.kg>
2025-04-28 15:19:26 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,11 +9,11 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       {/* 修改这里的内容 */}
-      <span className='mr-1'>power by</span>
+      <span className='mr-1'>行己所爱 </span>
       <a
         href='https://aze66.top'
         className='underline justify-start'>
-        行己所爱 {siteConfig('VERSION')}
+         爱己所行 {siteConfig('VERSION')}
       </a>
       .
     </div>
